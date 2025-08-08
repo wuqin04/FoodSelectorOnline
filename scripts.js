7f6e70e8418fadfefbc723a9ef131781076fbd17
wuqin04 <wuqin419@gmail.com>
2025-08-08 20:10:38 +0800

    Updated the list panel

--- scripts.js
+++ scripts.js
@@ -1,21 +1,33 @@
-const foods = ["Biji", "杂饭之家", "Desa", "MickeyMouse", "BlackBoard", "Wallace", "BlueZone Kopitiam",
-    "Taiwan Teahouse", "TianXia Kopitiam", "YaZhou Kopitiam", "MeiMei Kopitiam"]
+let restaurants = ["Biji", "杂饭之家", "Desa", "MickeyMouse", "BlackBoard", "Wallace", "BlueZone Kopitiam",
+    "Taiwan Teahouse", "TianXia Kopitiam", "YaZhou Kopitiam", "MeiMei Kopitiam"];
 
-function printFood() {
-    const foodList = document.querySelector(".list ol");
+function foodPanel() {
+    const restaurantsList = document.querySelector(".list ol");
+    let len = restaurants.length;
 
-    foods.forEach(name => {
+    for (let i = 0; i < len; i++) {
         const li = document.createElement("li");
-        li.textContent = name;
-        foodList.appendChild(li);
-    });
+
+        // text container
+        const span = document.createElement("span");
+        span.textContent = restaurants[i];
+        li.appendChild(span);
+
+        // delete button
+        const button = document.createElement("button");
+        button.textContent = "X";
+        li.appendChild(button);
+
+        // append both text and button into the list
+        restaurantsList.append(li);
+    }
 }
 
 function pickFood() {
-    const random = Math.floor(Math.random() * foods.length);
-    const selection = foods[random];
+    const random = Math.floor(Math.random() * restaurants.length);
+    const selection = restaurants[random];
 
     alert("The system picked " + selection + "!");
 }
 
-document.addEventListener("DOMContentLoaded", printFood);
+document.addEventListener("DOMContentLoaded", foodPanel);
