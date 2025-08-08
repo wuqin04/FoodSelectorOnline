077d7998697914e4227d73af969f0f635ab4c2fe
wuqin04 <wuqin419@gmail.com>
2025-08-08 17:28:52 +0800

    dynamicallly print food list

--- scripts.js
+++ scripts.js
@@ -1,9 +1,21 @@
 const foods = ["Biji", "杂饭之家", "Desa", "MickeyMouse", "BlackBoard", "Wallace", "BlueZone Kopitiam",
     "Taiwan Teahouse", "TianXia Kopitiam", "YaZhou Kopitiam", "MeiMei Kopitiam"]
 
+function printFood() {
+    const foodList = document.querySelector(".list ol");
+
+    foods.forEach(name => {
+        const li = document.createElement("li");
+        li.textContent = name;
+        foodList.appendChild(li);
+    });
+}
+
 function pickFood() {
     const random = Math.floor(Math.random() * foods.length);
     const selection = foods[random];
 
     alert("The system picked " + selection + "!");
 }
+
+document.addEventListener("DOMContentLoaded", printFood);
